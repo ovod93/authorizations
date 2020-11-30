4fa64101f30e6eb88c68750842f6a698c9d83d36
ovod93 <alexander.shein1993@gmail.com>
2020-12-01 00:12:09 +0700

    добавлена визуальная составляющая добавлено примерное подобие разделения кода на стили, воркеры добавлена заготовка под дб

---
+++ visual/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация</title>
+    <link rel="stylesheet" href="styles/css/main.css">
+</head>
+<body>
+<!-- Форма авторизации -->
+<form action="" method="GET">
+    <label>Login</label>
+    <input type="text" placeholder="Type your login">
+    <label>Password</label>
+    <input type="password" placeholder="Type your password">
+    <button>Sign in</button>
+</form>
+</body>
+</html>
