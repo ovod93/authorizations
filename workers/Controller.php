8bc04a5486d817d03110e090679293074b8596ab
ovod93 <alexander.shein1993@gmail.com>
2020-12-01 18:02:14 +0700

    настройка контроллера 3

--- workers/Controller.php
+++ workers/Controller.php
@@ -1,15 +1,3 @@
 <?php
 
-    function check($_POST) {
-        $login = $_POST['Login'];
-        $password = $_POST['Password'];
-    }
-
-    $dbSourse = 'authorizations/dataBase/db.csv';
-    $open = fopen($dbSourse, 'r');
-    dd($open);
-
-    function db_parser($open) {
-
-        $csvToArray = str_getcsv();
-    }
+    $fileName = "authorizations/dataBase/db.csv";
